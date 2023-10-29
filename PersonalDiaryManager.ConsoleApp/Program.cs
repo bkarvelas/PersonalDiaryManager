a500f87f5f9cfe093cc95986b8804013fc3f089a
Bill Karvelas <vaskarvelas@gmail.com>
2023-10-29 09:55:52 +0200

    Initial Commit

---
+++ PersonalDiaryManager.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalDiaryManager.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coreLogic = new PersonalDiaryManager.Domain.CoreLogic();
+
+            Console.WriteLine(coreLogic.GetHelloWorld());
+        }
+    }
+}
---
+++ PersonalDiaryManager.Domain/CoreLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDiaryManager.Domain
+{
+    public class CoreLogic
+    {
+        public string GetHelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
